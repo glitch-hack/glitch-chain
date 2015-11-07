881868b6e81be8cc05f1475f1cda5c59e54ab815
Mariusz Nowostawski <nowostawski@gmail.com>
2015-11-07 16:33:27 +0100

    testing re-deployment.

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -2,18 +2,18 @@
 
 DEPLOYMENT_PATH=/srv/glitch-chain/
 
-GIT_WORK_TREE=$DEPLOYMENT_PATH
+GIT_WORK_TREE="$DEPLOYMENT_PATH"
 
 git update-server-info
 
 cd $DEPLOYMENT_PATH
 #git checkout production
-git pull 
+GIT_WORK_TREE="$DEPLOYMENT_PATH" git pull 
 
 
 # re-execute NPM to refresh dependencies
 
-cd server
+cd "$DEPLOYMENT_PATH"\server
 npm install
 
 # should be good now
