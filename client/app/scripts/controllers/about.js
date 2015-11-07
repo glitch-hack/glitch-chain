18a0edbb240435a4b4fed8fd09d7d591fb7dd53a
Mariusz Nowostawski <nowostawski@gmail.com>
2015-11-07 17:42:18 +0100

    Initial skeleton for the client single-page application. BitcoinJS dependencies included.

---
+++ client/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name glitchChainApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the glitchChainApp
+ */
+angular.module('glitchChainApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name glitchChainApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the glitchChainApp
+ */
+angular.module('glitchChainApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
