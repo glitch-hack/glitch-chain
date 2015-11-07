347d9ac64ab5dc86928f77f8793926172fda6366
Justin Fay <fay@rapidratings.com>
2015-11-07 17:51:52 +0000

    It aint pretty but it works

---
+++ foobarww.js
@@ -0,0 +1,10 @@
+
+var foobar = {
+  base58: require('bs58'),
+  bitcoin: require('bitcoinjs-lib'),
+  ecurve: require('ecurve'),
+  BigInteger: require('bigi'),
+  Buffer: require('buffer-browserify')
+}
+
+module.exports = foobar
