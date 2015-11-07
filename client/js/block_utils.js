cdec40ca14d2131ec8ce20fa87bf133c1cc7c342
Mariusz Nowostawski <nowostawski@gmail.com>
2015-11-07 23:06:53 +0100

    Adding param conversion to/from strings.

--- client/js/block_utils.js
+++ client/js/block_utils.js
@@ -3,23 +3,27 @@
 //
 
 
+// 
 // generate random buffer
-function rng () {
-    return new Buffer('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
-}
-
-
+//
+// TODO: fix this to actually generate random things
+//
+//function rng () {
+//    return new Buffer('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
+//}
+// 
+// 
 // generate random keyPair
-var keyPair = bitcoin.ECPair.makeRandom({ rng: rng })
-var address = keyPair.getAddress()
-
+// var keyPair = bitcoin.ECPair.makeRandom({ rng: rng })
+// var address = keyPair.getAddress()
+//
 // debug
-console.log(keyPair, address);
+// console.log(keyPair, address);
 
 
 // Fetch transactions from block explorer calling `callback`
 // on response coerced to JSON.
-var getTransactions = function (address, callback) {
+function getTransactions(address, callback) {
     var request = new XMLHttpRequest();
     request.open(
         "get",
@@ -29,7 +33,7 @@ var getTransactions = function (address, callback) {
 
 }
 
-var findVouts = function(obj) {
+function findVouts(obj) {
     var props = [];
     var label = 'vout';
     for(var i = 0; i < obj.txs.length; i++) {
@@ -43,15 +47,48 @@ var findVouts = function(obj) {
         }
     }
     return props;
-};
+}
 
-var parseParams = function () {
-    // Parse the glitch params from a bitcoin transaction.
-    console.log(findVouts(JSON.parse(this.responseText)));
-};
+function params2string(a, b, c, d) {
+    var s = a + ' ' + b + ' ' + c + ' ' + d;
+    console.log(s);
+    return s;
+}
+
+function string2Params(s) {
+    var buf = s.split(' ');    
+    var intBuf = [];
+    buf.forEach(function (s) { intBuf.push(parseInt(s)); });
+    return intBuf;
+}
+
+function hex2bin(hex)
+{
+    var bytes = [], str;
+
+    for(var i=0; i< hex.length-1; i+=2)
+        bytes.push(parseInt(hex.substr(i, 2), 16));
+
+    return String.fromCharCode.apply(String, bytes);    
+}
+
+function bin2hex(s) {
+  var i, l, o = '', n;
+
+  s += '';
+
+  for (i = 0, l = s.length; i < l; i++) {
+    n = s.charCodeAt(i)
+      .toString(16);
+    o += n.length < 2 ? '0' + n : n;
+  }
+
+  return o;
+}
+
 
 function hex2a(hexx) {
-    var hex = hexx.toString();//force conversion
+    var hex = hexx.toString(); //force conversion
     var str = '';
     for (var i = 0; i < hex.length; i += 2)
         str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
