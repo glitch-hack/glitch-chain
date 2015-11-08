6beba61b28378428910cae16407f42ebf4fa8293
m-davis <md5g14@soton.ac.uk>
2015-11-08 11:08:20 +0000

    Reverted to non-buffer draw method

--- client/js/image_utils.js
+++ client/js/image_utils.js
@@ -15,22 +15,35 @@ function glitchClicked() {
 
 
 function tryGlitch(repetitions) {
-	var arrayOut = [];
-	var diff = null;
-	var parameters = null;
-	parameters = getRandomParameters();
-	doGlitch(originalCanvas, parameters);
-  	diff = compare(originalCanvas, canvas2);
-  	console.log(diff);
-	arrayOut.push(diff);
+	var originalCanvas = document.getElementById('glitch-canvas-1');
+	var canvas2 = document.getElementById('glitch-canvas-2');
+	setOriginalImage(originalCanvas);
+
+	var parameters = getRandomParameters();
+	displayParameters(parameters);
+
+	var buffer = doGlitch(originalCanvas, parameters);
+  var diff = compare(originalCanvas, canvas2);
+	document.getElementById('differenceValue').innerHTML = diff;
+	var diff2 = compare(canvas3, canvas2);
+	console.log(diff);
+	console.log(diff2);
 }
 
+function displayParameters(parameters)
+{
+		document.getElementById('amountValue').innerHTML = parameters['amount'];
+		document.getElementById('seedValue').innerHTML = parameters['seed'];
+		document.getElementById('iterationsValue').innerHTML = parameters['iterations'];
+		document.getElementById('qualityValue').innerHTML = parameters['quality'];
+}
 
 function getRandomParameters(){
 	var amountValue = Math.floor((Math.random() * 100) + 1);
 	var seedValue = Math.floor((Math.random() * 100) + 1);
 	var iterationsValue = Math.floor((Math.random() * 100) + 1);
 	var qualityValue = Math.floor((Math.random() * 100) + 1);
+
 	return {
 		amount: amountValue,
 		seed: seedValue,
@@ -54,8 +67,32 @@ function doGlitch(canvas, parameters) {
 	function drawGlitchedImageData(image_data) {
   		context.putImageData(image_data, 0, 0);
 	}
+
 	glitch(imageData, parameters, drawGlitchedImageData);
 }
+
+function doBufferedGlitch(source, width, height, parameters) {
+	console.log("do glitch:", parameters)
+
+	var buffer = document.createElement('canvas');
+	buffer.width = width;
+	buffer.height = height;
+
+	var bufferContext = buffer.getContext('2d');
+	var sourceContext = buffer.getContext('2d');
+
+	bufferContext.drawImage(source, 0, 0);
+	var imageData = bufferContext.getImageData(0, 0, width, height);
+
+	function drawGlitchedImageData(image_data) {
+  		bufferContext.putImageData(image_data, 0, 0);
+	}
+
+	glitch(imageData, parameters, drawGlitchedImageData);
+
+	return buffer;
+}
+
 
 
 function setOriginalImage(canvas) {
@@ -66,7 +103,7 @@ function setOriginalImage(canvas) {
 
 function copyCanvas(originalCanvas, targetCanvas){
 	var context = targetCanvas.getContext('2d');
-	targetCanvas.drawImage(originalCanvas, 0, 0);
+	context.drawImage(originalCanvas, 0, 0);
 }
 
 
