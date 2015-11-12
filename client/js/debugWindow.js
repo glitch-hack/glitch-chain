36705f77a1172517632443a5ef602a79fd03389e
Nick Ellis-Gowland <divinci@live.co.uk>
2015-11-12 16:32:42 +0000

    Added a debug window class and tests.

---
+++ client/js/debugWindow.js
@@ -0,0 +1,83 @@
+
+
+// ~~~~~~~~~~~~~~~~ Unstyled unstructured usage
+//
+//      var myDebugWindow = new DebugWindow('idOfMyDebugWindowDiv');
+//      myDebugWindow.WriteLine('hello');
+//
+//
+// ~~~~~~~~~~~~~~~~ BootStrap structure
+//
+//      var myDebugWindow = new DebugWindow('idOfMyDebugWindowDiv');
+//      var myDebugWindow.AddRowCssClass('row');
+//      var myDebugWindow.AddFirstColumnCssClass('col-md-2');
+//      var myDebugWindow.AddSecondColumnCssClass('col-md-10');
+//
+//      myDebugWindow.WriteLine('hello');
+//
+
+function DebugWindow(domDivId_debugMessagesOutputDiv) {
+    this.domDivId_debugMessagesOutputDiv = domDivId_debugMessagesOutputDiv;
+    this.RowClasses = [];
+    this.FirstColumnClasses = [];
+    this.LastColumnClasses = [];
+};
+
+DebugWindow.prototype = {
+    constructor: DebugWindow,
+    WriteLine: function(message) {
+
+        console.log(message);
+
+        //if(!this.domDivId_debugMessagesOutputDiv === 'undefined')
+        {
+            try {
+                var newRow = document.getElementById(this.domDivId_debugMessagesOutputDiv).appendChild(
+                    document.createElement("div"));
+
+                newRow.className = this.GetRowCssClassString();
+                    
+                var firstColumn = newRow.appendChild(
+                    document.createElement("div"));
+
+                firstColumn.className = this.GetFirstColumnCssClassString();
+                firstColumn.appendChild(document.createTextNode(new Date().toLocaleString()));
+
+                var secondColumn = newRow.appendChild(
+                    document.createElement("div"));
+                secondColumn.className = this.GetLastColumnCssClassString();
+                secondColumn.appendChild(document.createTextNode(message));
+            }
+            catch (err) {
+                console.log('Error creating dom :', err);
+            }
+        }
+    },
+    AddRowCssClass: function(className) {
+        this.RowClasses.push(className);
+    },
+    AddFirstColumnCssClass: function(className) {
+        this.FirstColumnClasses.push(className);
+    },
+    AddSecondColumnCssClass: function(className) {
+        this.LastColumnClasses.push(className);
+    },
+    GetRowCssClassString: function () {
+        if (this.RowClasses === undefined)
+            return "";
+
+        return this.RowClasses.join(" ");
+    },
+    GetFirstColumnCssClassString: function () {
+        if (this.FirstColumnClasses === undefined)
+            return "";
+
+        return this.FirstColumnClasses.join(" ");
+    },
+    GetLastColumnCssClassString: function () {
+        if (this.LastColumnClasses === undefined)
+            return "";
+
+        return this.LastColumnClasses.join(" ");
+    }
+}
